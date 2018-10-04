c047dd3d29071fc20d8e55bad22b174b9db6afac
Florin Lungu <me@florinlungu.it>
2018-10-04 21:35:48 +0200

    minor updates and integrations

--- app/src/main/java/it/floryn90/webapp/MainActivity.java
+++ app/src/main/java/it/floryn90/webapp/MainActivity.java
@@ -37,6 +37,13 @@ public class MainActivity extends Activity {
     private String mCameraPhotoPath = null;
     private long size = 0;
 
+    // Storage Permissions variables
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+      Manifest.permission.READ_EXTERNAL_STORAGE,
+      Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode != INPUT_FILE_REQUEST_CODE || mUploadMessage == null) {
@@ -53,7 +60,7 @@ public class MainActivity extends Activity {
         }
 
         if (data != null || mCameraPhotoPath != null) {
-            Integer count = 1;
+            Integer count = 0; //fix fby https://github.com/nnian
             ClipData images = null;
             try {
                 images = data.getClipData();
@@ -89,11 +96,27 @@ public class MainActivity extends Activity {
         }
     }
 
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
 
         webView = (WebView) findViewById(R.id.webView);
         webSettings = webView.getSettings();
